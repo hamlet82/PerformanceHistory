cdbd9de703c212351ca7bfd70c3ad82c4c7fb0d8
Derek Miller <hamlet82@users.noreply.github.com>
2016-06-23 18:11:25 -0400

    Update reformat_xml_broken.py
    Try this new script. I think it works properly, though I ignored movement IDs (not movement names, though).
    
    BTW, there's a weird concert at the end of the 1842 xml file.

--- performancehistory-broken/Scripts/reformat_xml_broken.py
+++ performancehistory-broken/Scripts/reformat_xml_broken.py
@@ -40,95 +40,106 @@ for g in files:
                 soloist_instruments_list = str.split(soloist_instruments,";")
                 soloist_roles_list = str.split(soloist_roles,";")
                 for x in range(0,len(soloists_list)):
-                    lines.append("                    <soloist>\n")
-                    lines.append("                        <soloistName>%s</soloistName>\n"%fixSpaces(soloists_list[x]))
-                    lines.append("                        <soloistInstrument>%s</soloistInstrument>\n"%fixSpaces(soloist_instruments_list[x]))
-                    lines.append("                        <soloistRoles>%s</soloistRoles>\n"%fixSpaces(soloist_roles_list[x]))
-                    lines.append("                    </soloist>\n")
+                    lines.append("<soloist>")
+                    lines.append("<soloistName>%s</soloistName>"%fixSpaces(soloists_list[x]))
+                    lines.append("<soloistInstrument>%s</soloistInstrument>"%fixSpaces(soloist_instruments_list[x]))
+                    lines.append("<soloistRoles>%s</soloistRoles>"%fixSpaces(soloist_roles_list[x]))
+                    lines.append("</soloist>")
             except:
-                lines.append("                    <soloist>\n")
-                lines.append("                        <soloistName>%s</soloistName>\n"%soloists)
-                lines.append("                        <soloistInstrument>%s</soloistInstrument>\n"%soloist_instruments)
-                lines.append("                        <soloistRoles>%s</soloistRoles>\n"%soloist_roles)
-                lines.append("                    </soloist>\n")
+                lines.append("<soloist>")
+                lines.append("<soloistName>%s</soloistName>"%soloists)
+                lines.append("<soloistInstrument>%s</soloistInstrument>"%soloist_instruments)
+                lines.append("<soloistRoles>%s</soloistRoles>"%soloist_roles)
+                lines.append("</soloist>")
         else:
-            lines.append("                    <soloist>\n")
-            lines.append("                        <soloistName>%s</soloistName>\n"%soloists)
-            lines.append("                        <soloistInstrument>%s</soloistInstrument>\n"%soloist_instruments)
-            lines.append("                        <soloistRoles>%s</soloistRoles>\n"%soloist_roles)
-            lines.append("                    </soloist>\n")
+            lines.append("<soloist>")
+            lines.append("<soloistName>%s</soloistName>"%soloists)
+            lines.append("<soloistInstrument>%s</soloistInstrument>"%soloist_instruments)
+            lines.append("<soloistRoles>%s</soloistRoles>"%soloist_roles)
+            lines.append("</soloist>")
 
     # reprint the unchanged entities
     def standardItems(p):
-        lines.append("        <id>%s</id>\n"%p.find('id').text)
-        lines.append("        <programID>%s</programID>\n"%p.find('programID').text)
-        lines.append("        <orchestra>%s</orchestra>\n"%p.find('orchestra').text)
-        lines.append("        <season>%s</season>\n"%p.find('season').text)
+        lines.append("<id>%s</id>"%p.find('id').text)
+        lines.append("<programID>%s</programID>"%p.find('programID').text)
+        lines.append("<orchestra>%s</orchestra>"%p.find('orchestra').text)
+        lines.append("<season>%s</season>"%p.find('season').text)
 
     # reprint unchanged concert info entities
     def concertInfo(c):
-        lines.append("            <eventType>%s</eventType>\n"%c.find('eventType').text)
-        lines.append("            <Location>%s</Location>\n"%c.find('Location').text)
-        lines.append("            <Venue>%s</Venue>\n"%c.find('Venue').text)
-        lines.append("            <Date>%s</Date>\n"%c.find('Date').text)
-        lines.append("            <Time>%s</Time>\n"%c.find('Time').text)
+        lines.append("<eventType>%s</eventType>"%c.find('eventType').text)
+        lines.append("<Location>%s</Location>"%c.find('Location').text)
+        lines.append("<Venue>%s</Venue>"%c.find('Venue').text)
+        lines.append("<Date>%s</Date>"%c.find('Date').text)
+        lines.append("<Time>%s</Time>"%c.find('Time').text)
 
     # reorder worksInfo children into works, each with conductor, composer, title, and soloists (if applicable)
     def sortWorksInfo(works):
         conductors = works.findall('worksConductorName')
         composerAndWork = works.findall('worksComposerTitle')
-        # movement = works.findall('worksMovement')
+        work_ids = works.findall('workID')
+        movements = works.findall('worksMovement')
+        #movement_ids = works.findall('movementID')
         soloists = works.findall('worksSoloistName')
         soloist_instruments = works.findall('worksSoloistInstrument')
         soloist_roles = works.findall('worksSoloistRole')
-        work_id = works.findall('workID')
-        # movement_id = works.findall('movementID')
         for x in range(0,len(conductors)):
             composer_work_separated = separateComposerWork(composerAndWork[x].text)
-            lines.append("            <work ID=\"%s\">\n"%work_id[x].text)
+            lines.append("<work ID=\"%s\">"%work_ids[x].text)
             if re.match(r'Intermission',composer_work_separated[0]):
-                lines.append("                <interval>%s</interval>\n"%composer_work_separated[0][:-1])
+                lines.append("<interval>%s</interval>"%composer_work_separated[0][:-1])
             else:
                 if composer_work_separated[0]:
-                    lines.append("                <composerName>%s</composerName>\n"%composer_work_separated[0])
+                    lines.append("<composerName>%s</composerName>"%composer_work_separated[0])
                 if composer_work_separated[1]:
-                    lines.append("                <workTitle>%s</workTitle>\n"%composer_work_separated[1])
-                # if movement[x].text:
-                #     lines.append("                <movement>%s</movement>\n"%movement[x].text)
+                    lines.append("<workTitle>%s</workTitle>"%composer_work_separated[1])
+                if len(movements) > 0:
+                    if movements[x].text:
+                        lines.append("<movement>%s</movement>"%movements[x].text)
                 if conductors[x].text:
-                    lines.append("                <conductorName>%s</conductorName>\n"%conductors[x].text)
+                    lines.append("<conductorName>%s</conductorName>"%conductors[x].text)
                 try:
                     if soloists[x].text:
-                        lines.append("                <soloists>\n")
+                        lines.append("<soloists>")
                         sortSoloistInfo(soloists[x].text, soloist_instruments[x].text,soloist_roles[x].text)
-                        lines.append("                </soloists>\n")
+                        lines.append("</soloists>")
                 except:
                     pass
-            lines.append("            </work>\n")
+            lines.append("</work>")
 
     # parse xml file and write new output, per functions above
     programs = root.findall('doc')
 
-    lines.append("<programs>\n")
+    lines.append('<programs>')
 
     for p in programs:
-        lines.append("    <program>\n")
+        lines.append("<program>")
         standardItems(p)
         for c in p.findall('concertInfo'):
-            lines.append("        <concertInfo>\n")
+            lines.append("<concertInfo>")
             concertInfo(c)
-            lines.append("        </concertInfo>\n")
-        lines.append("        <worksInfo>\n")
+            lines.append("</concertInfo>")
+        lines.append("<worksInfo>")
         sortWorksInfo(p.find('worksInfo'))
-        lines.append("        </worksInfo>\n")
-        lines.append("    </program>\n")
+        lines.append("</worksInfo>")
+        lines.append("</program>")
 
     lines.append("</programs>")
+    all_lines = []
+    for l in lines:
+        l = re.sub(r'&', r'&amp;', l)
+        all_lines.append(l)
+    text = ''.join(all_lines)
+    text = re.sub(r'><','>\n<',text)
+    text = re.sub(r'(<program>|</program>)',r'  \1',text)
+    text = re.sub(r'(<id>|<programID>|<orchestra>|<season>|<concertInfo>|</concertInfo>|<worksInfo>|</worksInfo>)',r'    \1',text)
+    text = re.sub(r'(<eventType>|<Location>|<Venue>|<Date>|<Time>|<work ID|</work>|<interval>)',r'      \1',text)
+    text = re.sub(r'(<composerName>|<workTitle>|<movement>|<conductorName>|<soloists>|</soloists>)',r'        \1',text)
+    text = re.sub(r'(<soloist>|</soloist>)',r'          \1',text)
+    text = re.sub(r'(<soloistName>|<soloistInstrument>|<soloistRoles>)',r'            \1',text)
 
     # create new xml file
     f = codecs.open(g, 'w','utf-8')
-    f.write('<?xml version="1.0" encoding="utf-8"?>\n\n')
-    for l in lines:
-        l = re.sub(r'&', r'&amp;', l)
-        f.write(l)
+    f.write('<?xml version="1.0" encoding="utf-8"?>\n')
+    f.write(text)
     f.close()
